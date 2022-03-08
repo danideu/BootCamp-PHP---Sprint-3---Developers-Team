9a2f43ac4e562551aadeee6ae5d1f9c507a655c5
danideu <danideu@gmail.com>
2022-03-08 15:47:48 +0100

    Añadimos funcionaildad Modificar Datos y Actualizar estado v1

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -27,22 +27,57 @@ class TaskController extends ApplicationController
         echo  __FILE__ . " ".  __FUNCTION__;
     }
 
+    public function getTask($id){
+        echo  __FILE__ . " ".  __FUNCTION__;
+        
+        $taskJsonModel = new TaskJsonModel(); 
+        $users = $taskJsonModel->listAllTask();
+        echo "ID: " . $id . "<br>"; 
+        var_dump($users);
+        foreach ($users as $user){
+            if ($user['idTareas'] == $id){
+                // var_dump($user);
+                return $user;
+            }
+        }
+        return null;
+    }
+
     public function updateTaskAction() {
         echo  __FILE__ . " ".  __FUNCTION__;
-        //Declaramos un objeto de tipo Model  
-        $taskJsonModel = new TaskJsonModel();   
-        // Pasamos los parametros del controller a la vista 
-        $users = $taskJsonModel->listAllTask();
-        $user = $taskJsonModel->updateTask($users);
-        var_dump($user);
+        echo "<br>UpdateTaskAction<br>";
+        $id = $_GET['idTarea'];
+        $user = $this->getTask($id);
+        $this->view->updatetask = $user;
+    }
 
-        // $this->view->updatetask
+    public function changestatus(){
+        echo  __FILE__ . " ".  __FUNCTION__;
+        $id = $_GET['idTarea'];
+        echo "ID: " . $id . "<br>"; 
+        $user = $this->getTask($id);
+        $taskJsonModel = new TaskJsonModel();       
+        $taskJsonModel->changeStatusTask($user);
+        // $this->view->showtask = $taskJsonModel->changeStatusTask();
+        // $this->view->updatetask = $user;
     }
 
     public function showTaskAction() {      
        //Declaramos un objeto de tipo Model  
-       $taskJsonModel = new TaskJsonModel();       
-       
+       $taskJsonModel = new TaskJsonModel();
+
+       //Chequeamos si es cambio de estado
+       if (isset($_GET['op'])){ 
+           $option = $_GET['op'];
+           if ($option == 'changestatus'){
+                echo "<p style=color:red>CHANGESTATUS</p>";
+                $id = $_GET['idTarea'];
+                echo "ID: " . $id . "<br>"; 
+                $user = $this->getTask($id);
+                $taskJsonModel = new TaskJsonModel();       
+                $taskJsonModel->changeStatusTask($user);
+            }
+       }
        // Pasamos los parametros del controller a la vista 
        $this->view->showtask = $taskJsonModel->listAllTask();
     }
