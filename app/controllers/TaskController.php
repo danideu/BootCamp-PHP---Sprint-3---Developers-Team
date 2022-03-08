e31cbdf9dfb50ef72801d288813935cada21a9d7
Judith Vilela <judith.vilela@gmail.com>
2022-03-09 00:04:17 +0100

    Create and delete task

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -14,17 +14,40 @@ class TaskController extends ApplicationController
 		echo "hello from test::check";
 	}
 
-    public function createTaskAction() {
-       echo  __FILE__ . " ".  __FUNCTION__;
-        /*
-        * Recoger los valores de los campos
-        * Validar la información contra bd o js
-        * llamar al modelo para guardar los datos del form
-        */
-    }
+    public function createTaskAction() {       
 
-    public function deleteTaskAction() {
-        echo  __FILE__ . " ".  __FUNCTION__;
+        if (isset($_POST['titulo']) || isset($_POST['descripcion'])) {
+            // Recoger los valores de los campos
+            $titulo = $_POST['titulo'];
+            $descripcion = $_POST['descripcion'];
+
+            // Validar la información contra bd o js
+            $task = new stdClass();               
+            //@TODO Cambiar por una clase Util     
+            $taskJsonModel = new TaskJsonModel(); 
+            $task->idTareas = $taskJsonModel->generateUuid();
+            $task->titulo = $titulo;
+            $task->descripcion = $descripcion;
+            $task->estado = TaskJsonModel::ESTADO_PDTE;            
+            $task->fec_creacion = $taskJsonModel->getDateFormat();
+            $task->fec_modif = "";
+            $task->fec_fintarea = "";
+
+            // Guardamos la task
+            $taskJsonModel->saveTask($task);     
+        }
+    }
+    
+    public function deleteTaskAction($idTask) {
+        // Obtenemos el objeto task
+        $task = $this->getTask($idTask);
+        
+        // Eliminamos la task seleccionada
+        $taskJsonModel = new TaskJsonModel();
+        $taskJsonModel->deleteTask($task);
+        
+        // Refrescamos la lista
+        $this->view->showtask = $taskJsonModel->listAllTask();
     }
 
     public function updateTaskAction() {
@@ -37,10 +60,48 @@ class TaskController extends ApplicationController
        
        // Pasamos los parametros del controller a la vista 
        $this->view->showtask = $taskJsonModel->listAllTask();
+
+       // En función de la operación a realizar, llamamos al método correspondiente
+       if (isset($_GET['op'])) {
+            $op = $_GET['op'];
+            $id = $_GET['id'];
+
+            echo 'Operacion:'. $op . '<br>';
+            echo 'idTarea:'. $id . '<br>';
+
+            switch($op) {
+                case 'create':
+                    $this->createTaskAction();
+                    break;     
+                case 'delete':
+                      $this->deleteTaskAction($id);
+                      break;                
+            }
+       }
     }
 
     public function changeStateTaskAction() {
         echo  __FILE__ . " ".  __FUNCTION__;
     }
 
+    public function getTask($id){
+        $taskJsonModel = new TaskJsonModel(); 
+        $users = $taskJsonModel->listAllTask();
+
+        /*echo "ID: " . $id . "<br>"; 
+        echo '<pre>';
+        print_r($users);
+        echo '</pre>';*/
+
+        foreach ($users as $user){
+            if ($user['idTareas'] == $id){
+                /*echo '<pre>';
+                print_r($user);
+                echo '</pre>';*/
+                return $user;
+            }
+        }
+        return null;
+    }
 }
+
--- app/models/TaskJsonModel.php
+++ app/models/TaskJsonModel.php
@@ -1,16 +1,72 @@
 <?php
 
+//include ('Ramsey\Uuid');
+
 class TaskJsonModel{
     private $json = "todo.json";
-    //private $tareas_json = null;
+    //private $tareas_json = null;    
+    public const ESTADO_PDTE = 'Pendiente';
+    public const ESTADO_COMP = 'Completado';
 
     public function __construct()
     {  
     }
+   
+    private function openFile(): string
+    {
+        return file_get_contents(ROOT_PATH . "/lib/db/json/todoTask.json", true);                   
+    }
 
-    public function saveTask($task): void{
-        echo 'Crear tarea';
-        var_dump($task);        
+    private function saveFile($data): void
+    {
+        file_put_contents(ROOT_PATH . "/lib/db/json/todoTask.json", json_encode($data));  
+    }
+    
+    public function generateUuid()
+    {
+        // Generamos el ID de la task
+        //$uuid = Uuid::uuid4();
+        //return $uuid->toString();/
+        return 12345; 
+    }
+
+    public function getDateFormat()
+    {
+        $date = new DateTime('now');
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public function saveTask($task): void{    
+        // Codificamos stdClass >> JSON
+       /* $taskJson = json_encode((array)$task);
+        echo 'NEW task JSON:';
+        print_r($taskJson);
+        echo '</pre>';*/
+        
+        // Abrimos nuestro fichero json        
+        $allTask = json_decode($this->openfile(), true);
+        /*echo 'ALL :';
+        echo '<pre>';
+        print_r($allTask);
+        echo '</pre>';
+
+        echo '******************* :';
+        echo '<pre>';
+        print_r(json_encode($allTask));
+        echo '</pre>';*/
+
+        // Añadimos la nueva task
+        array_push($allTask, $task);
+        
+        //$a=json_encode($allTask);
+        /*echo '******************* :';
+        echo '<pre>';
+        print_r($a);
+        echo '</pre>';*/
+
+
+        // Guardamos datos en fichero json        
+        $this->saveFile(json_encode($allTask));
     }
 
     public function listAllTask() {    
@@ -20,9 +76,34 @@ class TaskJsonModel{
         return json_decode($tareas_json, true);         
     }
 
-    public function deleteTask($task){
-        $this->task = $task;
-        echo 'Eliminar tarea' . $this->task;
+    public function deleteTask($task){  
+        
+        /*echo 'TASK:';
+        echo '<pre>';
+        print_r($task);
+        echo '</pre>';*/
+
+        // Abrimos nuestro fichero json        
+        $allTask = json_decode($this->openfile(), true);
+       
+        /*echo 'ALL :';
+        echo '<pre>';
+        print_r($allTask);
+        echo '</pre>';*/
+
+        // Buscamos task
+        foreach($allTask as $key=>$value) {
+            if($value['idTareas'] == $task['idTareas']) {
+                unset($allTask[$key]);
+            }    
+        }
+
+        // Guardamos datos en fichero json
+        /*echo 'SAVE:';
+        echo '<pre>';
+        print_r($allTask);
+        echo '</pre>';*/
+        $this->saveFile($allTask);
     }
 
     public function updateTask($task){
